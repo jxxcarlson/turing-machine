039969d11d631f6182d347d5b92dfbfe19e244ed
James Carlson <jxxcarlson@gmail.com>
2020-10-04 18:06:55 -0400

    Use Control.Monad.sequence_ for proper behavior

--- app/Main.hs
+++ app/Main.hs
@@ -8,11 +8,14 @@ import Machine.EnumerateN
 
 main :: IO ()
 main =
-    do 
-      drainFrom $ forM [0..100] (\n -> do putStrLn $ (show n ++ ": " ++ show (advanceAndShow'  n)) )
+    let
+        actions :: [IO()]
+        actions = map (\n -> putStrLn $ (show n ++ ": " ++ show (advanceAndShow'  n)) ) [0..100]
+    in
+      do 
+          sequence_ actions
 
+advanceAndShow n = advanceN machine n initalConfiguration |> showConfig 30
 
-advanceAndShow n = advanceN machine  n initalConfiguration |> showConfig 30
-
-advanceAndShow' n = advanceN  machine n initalConfiguration |> showConfig' 30
+advanceAndShow' n = advanceN machine n initalConfiguration |> showConfig' 30
 
